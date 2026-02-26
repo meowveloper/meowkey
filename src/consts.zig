9f8153e0388eeac4ef85ffbcfc24080d2d85647b
meowveloper <meowveloper.dev@gmail.com>
2026-02-27 03:52:34 +0700

    fix config bin path

--- src/consts.zig
+++ src/consts.zig
@@ -1,5 +1,5 @@
 pub const SOUND_FILE_PATH = "~/.config/meowkey/sound.wav";
 pub const CONFIG_FILE_PATH = "~/.config/meowkey/config.json";
-pub const CONFIG_BIN_PATH = "/tmp/meowkey/config.bin";
+pub const CONFIG_BIN_PATH = "~/.cache/meowkey/config.bin";
 
 pub const AudioError = error{ OpenFailed, ParamSetupFailed, InvalidHeader, NotARiffFile, NotAWaveFile, UnsupportedCompression, Only16BitSupported, IncompleteData };
